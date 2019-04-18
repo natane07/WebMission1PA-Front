2ade6c4c02f9f5edc02dc4e74871bdc490b8a0d0
TheGhostSpirit <quentin.caritey@outlook.fr>
2019-04-18 12:17:21 +0200

    header routing now works properly

--- src/views/header/header.js
+++ src/views/header/header.js
@@ -1,11 +1,18 @@
 import { inject } from 'aurelia-framework';
 import { Connection } from 'plugin/connection';
+import { Router } from 'aurelia-router';
+import { SITEMAP } from '../../config/app-config';
 
-@inject(Connection)
+@inject(Connection, Router)
 export class Header {
 
-  constructor(connection) {
+  constructor(connection, router) {
     this.connection = connection;
+    this._router = router;
+  }
+
+  goToSettings() {
+    this._router.navigateToRoute(SITEMAP.settings);
   }
 
 }
