e80a754045153e6990cf54089345d12bbacc7cdc
TheGhostSpirit <quentin.caritey@outlook.fr>
2019-05-29 10:43:46 +0200

    improved article details view

--- src/views/article/article.js
+++ src/views/article/article.js
@@ -1,15 +1,22 @@
 import { inject } from 'aurelia-framework';
 import { ArticleService } from 'services/article-service';
+import { Router } from 'aurelia-router';
+import { SITEMAP } from 'config/app-config';
 
-@inject(ArticleService)
+@inject(ArticleService, Router)
 export class Article {
 
-  constructor(articleService) {
+  constructor(articleService, router) {
     this._articleService = articleService;
+    this._router = router;
   }
 
   activate(params) {
     this._articleService.getArticle(params.id).then(res => (this.article = res));
+  }
+
+  backToArticles() {
+    return this._router.navigateToRoute(SITEMAP.stock);
   }
 
 }
